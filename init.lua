9fa285cd96259e692fb8aa9b7240d7dc6dcf05e2
Dominic DeWolfe <domdewolfe@gmail.com>
2024-07-11 09:57:07 -0400

    add chat

--- init.lua
+++ init.lua
@@ -50,6 +50,19 @@ vim.opt.rtp:prepend(lazypath)
 local plugins = {
 
   {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    branch = "canary",
+    dependencies = {
+      { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
+      { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+    },
+    opts = {
+      debug = true, -- Enable debugging
+      -- See Configuration section for rest
+    },
+    -- See Commands section for default commands if you want to lazy load on them
+  },
+  {
     "9seconds/repolink.nvim",
     dependencies = {
       "nvim-lua/plenary.nvim"
